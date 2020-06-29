173eb47c568a8420c60b5fc989213c67e946cab4
Jonathan <johnzballad@gmail.com>
2020-06-29 11:50:11 -0300

    Registering UserType

--- server/src/modules/node/definitions.js
+++ server/src/modules/node/definitions.js
@@ -1,5 +1,6 @@
 import { nodeDefinitions, fromGlobalId } from 'graphql-relay';
 
+import UserModel from '../user/UserModel';
 import ItemModel from '../item/ItemModel';
 
 export const { nodeInterface, nodeField } = nodeDefinitions(
@@ -9,11 +10,15 @@ export const { nodeInterface, nodeField } = nodeDefinitions(
     if (type === 'Item') {
       return ItemModel.findById(id);
     }
+    if (type === 'User') {
+      return UserModel.findById(id);
+    }
 
     return null;
   },
   obj => {
     if (obj.type) return 'Item';
+    if (obj.email) return 'User';
 
     return null;
   },
