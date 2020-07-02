347c1063415d30ace8acff6935756bce4abe67a8
Jonathan <johnzballad@gmail.com>
2020-07-02 16:54:46 -0300

    ListItem page

---
+++ web/src/pages/ListItem/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ListItem = () => {
+  return (
+    <Container>
+      <h1>Items list</h1>
+    </Container>
+  )
+}
+
+export default ListItem;
