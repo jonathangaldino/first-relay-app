a5a105650a0d7c38f1b96b51ed3226f5c8e50a47
Jonathan <johnzballad@gmail.com>
2020-07-03 09:46:06 -0300

    fetching nodes using the default query renderer

--- web/src/pages/ItemList/index.js
+++ web/src/pages/ItemList/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { createFragmentContainer } from 'react-relay';
+import { createFragmentContainer, QueryRenderer } from 'react-relay';
 import graphql from 'babel-plugin-relay/macro';
 
 import { Container, Content, List } from './styles';
-import createQueryRenderer from '../../relay/createQueryRendererModern';
+import Environment from '../../relay/Environment';
+// import createQueryRenderer from '../../relay/createQueryRendererModern';
+import Item from '../../components/Item';
 
 
-const ItemList = (props) => {  
+const ItemList = ({ items }) => {  
+  
   return (
     <Container>
       <Content>
         <h1>Items</h1>
 
         <List>
-          {/* {items.map(item => <Item key={item.id} item={item} />)} */}
+          { items.edges.map(item => <Item key={item.node.__id} item={item.node} />) }
         </List>
       </Content>
     </Container>
@@ -32,22 +35,44 @@ const ItemListFragmentContainer = createFragmentContainer(ItemList, {
   `
 })
 
-const ItemListQR = createQueryRenderer(
-  ItemListFragmentContainer,
-  ItemList,
-  {
-    query: graphql`
+// const ItemListQR = createQueryRenderer(
+//   ItemListFragmentContainer,
+//   ItemList,
+//   {
+//     query: graphql`
+//       query ItemListQuery {
+//         items {
+//           ...ItemList_items
+//         }
+//       }
+//     `,
+//   },
+// );
+
+
+const ItemListQueryRenderer = () => (
+  <QueryRenderer
+    environment={Environment}
+    query={graphql`
       query ItemListQuery {
         items {
           ...ItemList_items
         }
       }
-    `,
-    getFragmentProps: (({ items }) => ({
-      items,
-    })),
-  },
-);
+    `}
+    render={({ error, props }) => {
+      if (error) {
+        return <span>{error.toString()}</span>;
+      }
+
+      if (props) {
+        return <ItemListFragmentContainer {...props} query={props} />;
+      }
+
+      return <span>loading</span>;
+    }}
+  />
+)
 
 
-export default ItemListQR;
+export default ItemListQueryRenderer;
